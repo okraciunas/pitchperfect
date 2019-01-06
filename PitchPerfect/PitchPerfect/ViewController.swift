554a3f106722b865bb3ff32f5576161760984808
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2019-01-06 20:17:22 -0200

    Criação do projeto. Adicionando assets.

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Leonardo Oliva Kraciunas on 06/01/19.
+//  Copyright © 2019 Leonardo Oliva Kraciunas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
